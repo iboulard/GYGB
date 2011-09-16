5609c03c81c1893475a7649e8f788f547e62ca83
Pat Haggerty <haggertypat@gmail.com>
2011-09-16 13:47:11 -0400

    local copy set up

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+        <title>Get Your GreenBack Tompkins</title>
+        <link rel="stylesheet" href="styles.css" type="text/css">
+          
+    </head>
+    
+    <body>
+        Take a step to Get Your GreenBack!
+    </body>
+    
+</html>
