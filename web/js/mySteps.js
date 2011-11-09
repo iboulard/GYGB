882e4784c0f15cea1af8acba09f67133efe39efb
Pat Haggerty <haggertypat@gmail.com>
2011-11-09 16:02:05 -0500

    added tinymce to mystepsedit

---
+++ web/js/mySteps.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() {
+
+tinyMCE.init({
+        // General options
+        mode : "specific_textareas",
+        editor_selector : "tinymce",
+        theme : "advanced",
+        plugins : ",advimage,iespell,inlinepopups,insertdatetime,paste",
+
+        theme_advanced_buttons1 : "bold,italic,underline,strikethrough",
+        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,image",
+        theme_advanced_buttons3 : "",
+        theme_advanced_buttons4 : "",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_resizing : true
+
+  
+});
+
+     
+});
+
